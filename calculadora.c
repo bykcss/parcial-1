19f56254d34647ba75951af96502957d980e28d9
Alejandro Méndez <business.sorii@gmail.com>
2026-06-14 15:20:20 -0600

    Avance 1.2 plantilla inicial

---
+++ calculadora.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void suma();
+float resta(float,float);
+void resta();
+float divison(float, float);
+
+int main() {
+    float a,b;
+    printf("calculadora sencilla ");
+    return 0;
+}
+
+void suma() {
+
+}
+
+float resta(float a, float b) {
+
+    return 0;
+}
+
+void resta() {
+
+}
+
+float divison(float a, float b) {
+    
+    return 0;
+}
