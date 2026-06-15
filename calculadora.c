581e599454f59e4b7e4f0c964370a42d266bc64b
Misael0435 <misael.gloria@u-erre.mx>
2026-06-14 18:50:45 -0600

    Se agregan funciones de multiplicacion y division - Misael

--- calculadora.c
+++ calculadora.c
@@ -25,6 +25,18 @@ void resta() {
 }
 
 float divison(float a, float b) {
-    
-    return 0;
+    if (b == 0) {
+        printf("Error: division entre cero\n");
+        return 0;
+    }
+    return a / b;
+}
+ 
+void multiplicacion() {
+    float a, b;
+    printf("Dame el primer numero: ");
+    scanf("%f", &a);
+    printf("Dame el segundo numero: ");
+    scanf("%f", &b);
+    printf("El resultado de la multiplicacion es: %.2f\n", a * b);
 }
